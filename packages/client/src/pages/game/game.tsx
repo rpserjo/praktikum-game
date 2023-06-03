f97aab27f1dd0cf8795d5b7b7454b9fea5beaac4
Maksim Popov <makso16108@gmail.com>
2023-06-03 17:57:21 +0300

    ROOMS-26 добавлена первоначальная разметка страницы.

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,5 +1,14 @@
 import React, { FC } from 'react';
+import style from './canvas.module.scss';
 
-const Game: FC = () => <h1>Game</h1>;
+const Game: FC = () => (
+    <div className={style.gamePage}>
+        <h1 className={style.title}>Одиночная игра</h1>
+
+        <div className={style.canvasWindow}>
+            <canvas width={900} height={420} />
+        </div>
+    </div>
+);
 
 export default Game;
