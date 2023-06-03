ac438987d4f94af015e05de223f889b50ae9b70c
Oleg Peunov <peunov@uchi.ru>
2023-06-03 15:07:41 +0300

    canvas width 900px - test

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,14 +1,25 @@
 import React, { FC, useRef, useEffect } from 'react';
+import style from './canvas.module.scss';
 
 const Game: FC = () => {
     const ref = useRef<HTMLCanvasElement | null>(null);
+
     useEffect(() => {
         if (ref.current) {
             const ctx = ref.current.getContext('2d');
-            ctx!.fillRect(0, 0, 100, 100);
+            ctx!.fillStyle = '#265B8F';
+            ctx!.fillRect(0, 0, 900, 420);
         }
     }, []);
-    return <canvas ref={ref} width={300} height={300} />;
+
+    return (
+        <>
+            <h1 className={style.title}>Одиночная игра</h1>
+            <div className={style.canvasWindow}>
+                <canvas ref={ref} width={900} height={420} />
+            </div>
+        </>
+    );
 };
 
 export default Game;
