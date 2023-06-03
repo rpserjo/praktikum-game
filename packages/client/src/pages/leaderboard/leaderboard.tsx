acf8aa554e5d9ead2f7c64076844c320fe898a26
Ruslan Belyi <baiso@ya.ru>
2023-06-03 19:22:41 +0300

    Add pagination for data, move mock data from mock-server.ts

--- packages/client/src/pages/leaderboard/leaderboard.tsx
+++ packages/client/src/pages/leaderboard/leaderboard.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import style from './leaderboard.module.scss';
-import MockServer from '../../mocks/mock-server';
+import MockServer from '@/mocks/mock-server';
 
 const Leaderboard: FC = () => {
     const { page } = useParams();
 
     const server = new MockServer();
-    const items = server.getLeaderBoardData();
+    const data = server.getLeaderBoardData();
+    const items = data.slice((+page! - 1) * 10, +page! * 10);
 
     const isShowPrev = page && +page > 1 ? true : undefined;
+
+    const lastPage = Math.ceil(data.length / 10);
+    const isShowNext = page && +page < lastPage ? true : undefined;
 
     return (
         <main className={style.main}>
@@ -18,7 +22,6 @@ const Leaderboard: FC = () => {
                 <table className={style['table-board']}>
                     <tr className={style.tr}>
                         <th className={style.td}>Имя</th>
-                        <th className={style.td}>E-mail</th>
                         <th className={style.td}>Логин</th>
                         <th className={style.td}>Побед</th>
                         <th className={style.td}>Поражений</th>
@@ -27,7 +30,6 @@ const Leaderboard: FC = () => {
                     {items.map(item => (
                         <tr className={style.tr}>
                             <td className={style.td}>{item.name}</td>
-                            <td className={style.td}>{item.email}</td>
                             <td className={style.td}>{item.login}</td>
                             <td className={style.td}>{item.winsCount}</td>
                             <td className={style.td}>{item.lostCount}</td>
@@ -42,9 +44,11 @@ const Leaderboard: FC = () => {
                         </Link>
                     )}
                     <span>{`< ${page} >`}</span>
-                    <Link className={style.link} to={`/leaderboard/${Number(page) + 1}`}>
-                        Next
-                    </Link>
+                    {isShowNext && (
+                        <Link className={style.link} to={`/leaderboard/${Number(page) + 1}`}>
+                            Next
+                        </Link>
+                    )}
                 </div>
             </div>
         </main>
