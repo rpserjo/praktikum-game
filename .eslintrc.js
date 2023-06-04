a75c4e59c2bd8cfe7a9b415d7f0082ea4ad9a0e0
Maksim Popov <makso16108@gmail.com>
2023-06-04 20:57:25 +0300

    ROOMS-26 изменил настройки максимальной длины строки в линтере и преттиере.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: ['@typescript-eslint'],
     rules: {
-        'max-len': ['error', { code: 200 }],
+        'max-len': ['error', { code: 100 }],
         'react/jsx-boolean-value': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
         indent: ['error', 4],
