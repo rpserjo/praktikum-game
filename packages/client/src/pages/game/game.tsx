eb5e8c8ed1a275d7d69d22456bec3173a75d0a66
Oleg Peunov <peunov@uchi.ru>
2023-06-04 20:28:41 +0300

    main scene update

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -7,8 +7,24 @@ const Game: FC = () => {
     useEffect(() => {
         if (ref.current) {
             const ctx = ref.current.getContext('2d');
-            ctx!.fillStyle = '#265B8F';
-            ctx!.fillRect(0, 0, 900, 420);
+
+            // ctx!.strokeStyle = "white";
+            // ctx!.lineWidth = 2;
+            // ctx!.strokeRect(0, 0, 30, 30);
+
+            // eslint-disable-next-line
+            const roundRect = function <T extends number>(x: T, y: T, width: T, height: T, radius: T): void {
+                ctx!.beginPath();
+                ctx!.moveTo(x + radius, y);
+                ctx!.arcTo(x + width, y, x + width, y + height, radius);
+                ctx!.arcTo(x + width, y + height, x, y + height, radius);
+                ctx!.arcTo(x, y + height, x, y, radius);
+                ctx!.arcTo(x, y, x + width, y, radius);
+                ctx!.closePath();
+                ctx!.fillStyle = '#265B8F';
+                ctx!.fill();
+            };
+            roundRect(200, 0, 800, 400, 10);
         }
     }, []);
 
@@ -16,7 +32,7 @@ const Game: FC = () => {
         <>
             <h1 className={style.title}>Одиночная игра</h1>
             <div className={style.canvasWindow}>
-                <canvas ref={ref} width={900} height={420} />
+                <canvas ref={ref} width={1200} height={400} />
             </div>
         </>
     );
