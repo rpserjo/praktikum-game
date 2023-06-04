63d5f01ddc6656bfdc2c2a38b884307a1bdaaeaf
Maksim Popov <makso16108@gmail.com>
2023-06-04 20:23:02 +0300

    ROOMS-26 Добавлено модальное окно на страницу Game, которое появляется при окончании игры, а также стили и обработка клика по кнопкам модального окна.

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,18 +1,37 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEventHandler } from 'react';
+import cn from 'classnames';
+import { useNavigate } from 'react-router-dom';
 import style from './game.module.scss';
 import Button from '../../components/ui/button/button';
 import UserIcon from '../../components/ui/userIcon/userIcon';
 import Ships from '../../components/ui/ships/ships';
 
-// todo: использование пропсов - временное решение. Необходимо заменить на использование глобального состояния, когда начнем его использовать.
+// todo: использование пропсов - временное решение.
+//  Необходимо заменить на использование глобального состояния, когда начнем его использовать.
 type TGame = {
     mode?: string;
     move?: string;
     shipsCount?: number;
+    gameOver?: string;
 };
 
 const Game: FC<TGame> = props => {
-    const { mode = 'battle', move = 'user', shipsCount = 10 } = props;
+    const { mode = 'battle', move = 'user', shipsCount = 10, gameOver } = props;
+    const navigate = useNavigate();
+
+    const handleWinButtonClick: MouseEventHandler<HTMLButtonElement> = event => {
+        event.preventDefault();
+        navigate('/game');
+    };
+
+    const handleDefeatButtonClick: MouseEventHandler<HTMLButtonElement> = event => {
+        event.preventDefault();
+        navigate('/game');
+    };
+
+    const endGameModalClasses = cn(style.endGameModal, {
+        [style.active]: !!gameOver,
+    });
 
     return (
         <div className={style.gamePage}>
@@ -26,7 +45,10 @@ const Game: FC<TGame> = props => {
                 <div className={style.leftSide}>
                     {mode === 'battle' ? (
                         <>
-                            <UserIcon type="game" userData={{ firstName: 'Иск.', secondName: 'Интеллект' }} />
+                            <UserIcon
+                                type="game"
+                                userData={{ firstName: 'Иск.', secondName: 'Интеллект' }}
+                            />
                             <Ships mode={mode} position="left" />
                         </>
                     ) : null}
@@ -38,16 +60,48 @@ const Game: FC<TGame> = props => {
                     </div>
 
                     <div className={style.userInfoBlock}>
-                        {mode === 'battle' && move === 'user' ? <span className={style.gameMessage}>Ваш ход!</span> : null}
-                        {mode === 'battle' && move === 'enemy' ? <span className={style.gameMessage}>Ход противника</span> : null}
-                        {mode === 'placement' && shipsCount ? <span className={style.gameMessage}>Расставьте корабли</span> : null}
-                        {mode === 'placement' && !shipsCount ? <Button buttonSize="medium">Готов к бою!</Button> : null}
+                        {mode === 'battle' && move === 'user' ? (
+                            <span className={style.gameMessage}>Ваш ход!</span>
+                        ) : null}
+
+                        {mode === 'battle' && move === 'enemy' ? (
+                            <span className={style.gameMessage}>Ход противника</span>
+                        ) : null}
+
+                        {mode === 'placement' && shipsCount ? (
+                            <span className={style.gameMessage}>Расставьте корабли</span>
+                        ) : null}
+
+                        {mode === 'placement' && !shipsCount ? (
+                            <Button buttonSize="medium">Готов к бою!</Button>
+                        ) : null}
                     </div>
                 </div>
 
                 <div className={style.rightSide}>
                     <UserIcon type="game" />
                     <Ships mode={mode} isUserShips={true} />
+                </div>
+            </div>
+
+            <div className={endGameModalClasses}>
+                <div className={style.modalContent}>
+                    <div className={style.modalTitle}>
+                        {gameOver === 'win' && 'Вы победили!'}
+                        {gameOver === 'defeat' && 'Вы проиграли :('}
+                    </div>
+
+                    {gameOver === 'win' ? (
+                        <Button buttonSize="large" onClick={handleWinButtonClick}>
+                            Ура!
+                        </Button>
+                    ) : null}
+
+                    {gameOver === 'defeat' ? (
+                        <Button onClick={handleDefeatButtonClick} buttonSize="large">
+                            Угу
+                        </Button>
+                    ) : null}
                 </div>
             </div>
         </div>
