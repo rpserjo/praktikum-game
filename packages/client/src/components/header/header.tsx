71c0df52034647214c3a8fd8a8054f77bd705ffb
rpserjo <rpserjo@gmail.com>
2023-06-06 20:29:33 +0300

    Feature/rooms 28 (#9)
    * Добавлено лого, ссылки теперь через <Navlink>
    
    * fix

--- packages/client/src/components/header/header.tsx
+++ packages/client/src/components/header/header.tsx
@@ -1,19 +1,34 @@
 import React, { FC, ReactElement } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { Button } from '@ui';
 import style from './header.module.scss';
+import Logo from '@/assets/logo.svg';
 
 type HeaderProps = {}; // eslint-disable-line
 
 const Header: FC<HeaderProps> = (): ReactElement => (
     <div className={style.header}>
-        <div className={style.logo}>ЛОГО</div>
+        <div className={style.logo}>
+            <Link to="/">
+                <img alt="Главная" src={Logo} />
+            </Link>
+        </div>
         <div className={style.navigation}>
-            <Link to="/">Landing</Link>
-            <Link to="/signup">Sign up</Link>
-            <Link to="/signin">Sign in</Link>
-            <Link to="/game">Game</Link>
-            <Link to="/leaderboard/1">Leaderboard</Link>
-            <Link to="/forum">Forum</Link>
+            <div className={style.links}>
+                <NavLink to="/forum" className={({ isActive }) => (isActive ? style.active : '')}>
+                    Форум
+                </NavLink>
+                <NavLink to="/leaderboard/1" className={({ isActive }) => (isActive ? style.active : '')}>
+                    Лидерборд
+                </NavLink>
+                <NavLink to="/profile" className={({ isActive }) => (isActive ? style.active : '')}>
+                    Профиль
+                </NavLink>
+            </div>
+            <div className={style.user}>USER</div>
+            <Button buttonSize="medium" onClick={() => console.log('Sign out')}>
+                Выйти
+            </Button>
         </div>
     </div>
 );
