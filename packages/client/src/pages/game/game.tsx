f5fa471c08bc094a500979aeccccfca8655c4c9f
Oleg Peunov <olegpeunov@yandex.ru>
2023-06-07 19:30:04 +0300

    horisontal text

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -34,9 +34,64 @@ const Game: FC = () => {
                     ctx!.strokeRect(x, y + 30 * index, 300, 30);
                 }
             };
+
             renderBattlefield(650, 70);
             renderBattlefield(250, 70);
 
+            // text render
+            // eslint-disable-next-line
+            function fillTextWithSpacing(context: CanvasRenderingContext2D, text: string, x: number, y: number, spacing: number) {
+                const totalWidth = context.measureText(text).width + spacing * (text.length - 1);
+
+                const align = context.textAlign;
+                context.textAlign = 'left';
+                /* eslint-disable */
+                switch (align) {
+                    case 'right':
+                        x -= totalWidth;
+                        break;
+                    case 'center':
+                        x -= totalWidth / 2;
+                        break;
+                }
+                /* eslint-enable */
+
+                let offset;
+                let pairWidth;
+                let charWidth;
+                let charNextWidth;
+                let pairSpacing;
+                let char;
+                let charNext;
+
+                for (offset = 0; offset < text.length; offset += offset + 1) {
+                    char = text.charAt(offset);
+                    pairSpacing = 0;
+                    if (offset + 1 < text.length) {
+                        charNext = text.charAt(offset + 1);
+
+                        pairWidth = context.measureText(char + charNext).width;
+                        charWidth = context.measureText(char).width;
+                        charNextWidth = context.measureText(charNext).width;
+                        pairSpacing = pairWidth - charWidth - charNextWidth;
+                    }
+
+                    context.fillText(char, x, y);
+                    if (charWidth) {
+                        // eslint-disable-next-line
+                        x = x + pairSpacing + spacing + charWidth;
+                    }
+                }
+
+                context.textAlign = align;
+            }
+            ctx!.font = '19px inter';
+            ctx!.fillStyle = 'white';
+            const text = 'A B C D E F G H I G';
+            // eslint-disable-next-line
+            [...Array(2).keys()].forEach(i => fillTextWithSpacing(ctx!, text, 257 + 400 * i, 63, 6.6));
+
+            // render ships
             // eslint-disable-next-line
             [...Array(4).keys()].forEach(i => {
                 const image = new Image();
