200502ed294d2c7960218473f2245c1b4ae9695d
Olga Kuptsova <helllga2018@yandex.ru>
2023-06-09 09:10:45 +0400

    feature/ROOMS-17_validation_refactored_rule_type_added

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,7 @@ module.exports = {
         es2020: true,
         node: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'airbnb',
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'airbnb'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 11,
@@ -25,15 +20,14 @@ module.exports = {
         'no-console': 'off',
         'import/no-unresolved': 'off',
         'import/extensions': 'off',
-        'react/jsx-filename-extension': [
-            2,
-            { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-        ],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
         'react/function-component-definition': 'off',
         'react/button-has-type': 'off',
         'react/require-default-props': 'off',
         'object-curly-newline': 'off',
         'linebreak-style': 'off',
         'arrow-parens': ['error', 'as-needed'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
     },
 };
--- packages/client/src/utils/validationService.ts
+++ packages/client/src/utils/validationService.ts
@@ -3,65 +3,35 @@ export interface IValidationResult {
     isValid: boolean;
 }
 
-type ValidationInfo = {
-    error: string;
-    regex: RegExp;
-};
+// eslint-disable-next-line
+export enum RuleNames {
+    NAME,
+    LOGIN,
+    PASSWORD,
+    EMAIL,
+    PHONE,
+}
 
 class ValidationService {
-    // eslint-disable-next-line
-    private _validate(value: string, info: ValidationInfo): IValidationResult {
+    private _rules = {
+        [RuleNames.NAME]: { error: 'Первая буква должна быть заглавной, не может содержать цифры, пробелы и спецсимволы', regex: /^[A-Z]{1}[A-za-z-]*$/ },
+        [RuleNames.LOGIN]: { error: 'Длина от 3 до 20 символов, не может содержать цифры, пробелы и спецсимволы', regex: /(?=.*[a-zA-Z])[a-zA-Z0-9_-]{3,20}/ },
+        [RuleNames.PASSWORD]: { error: 'Длина от 8 до 40 символов, должен содержать хотя бы одну цифру или заглавную букву', regex: /^(?=.*\d)(?=.*[A-Z]).{8,40}$/ },
+        [RuleNames.EMAIL]: {
+            error: 'Невалидный email',
+            // eslint-disable-next-line
+            regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        },
+        [RuleNames.PHONE]: { error: 'Длина от 10 до 15 символов, может содержать только цифры или + в начале', regex: /^((\+[0-9])|([0-9])){10,15}$/ },
+    };
+
+    public validateInput(type: RuleNames, value: string): IValidationResult {
+        // eslint-disable-next-line
+        const info = this._rules[type];
         return {
             errorMessage: info.error,
             isValid: info.regex.test(value),
         };
-    }
-    // eslint-disable-next-line
-    private _getValidationInfo(name: string): ValidationInfo {
-        switch (name) {
-            case 'first_name':
-            case 'second_name':
-            case 'display_name':
-                return {
-                    error: 'Первая буква должна быть заглавной, не может содержать цифры, пробелы и спецсимволы',
-                    regex: /^[A-Z]{1}[A-za-z-]*$/,
-                };
-            case 'login':
-                return {
-                    error: 'Длина от 3 до 20 символов, не может содержать цифры, пробелы и спецсимволы',
-                    regex: /(?=.*[a-zA-Z])[a-zA-Z0-9_-]{3,20}/,
-                };
-            case 'password':
-                return {
-                    error: 'Длина от 8 до 40 символов, должен содержать хотя бы одну цифру или заглавную букву',
-                    regex: /^(?=.*\d)(?=.*[A-Z]).{8,40}$/,
-                };
-            case 'email':
-                return {
-                    error: 'Невалидный email',
-                    // eslint-disable-next-line
-                    regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                };
-            case 'phone':
-                return {
-                    error: 'Длина от 10 до 15 символов, может содержать только цифры или + в начале',
-                    regex: /^((\+[0-9])|([0-9])){10,15}$/,
-                };
-            default:
-                throw new Error('No validation rule is found for the field');
-        }
-    }
-
-    public validateInput(name: string, value: string): IValidationResult | null {
-        try {
-            // eslint-disable-next-line
-            const info = this._getValidationInfo(name);
-            // eslint-disable-next-line
-            return this._validate(value, info);
-        } catch (error: unknown) {
-            console.log((error as Error).message);
-        }
-        return null;
     }
 }
 
