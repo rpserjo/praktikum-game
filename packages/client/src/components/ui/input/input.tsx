d7a90a1cf4c76b8d6611c25c03cdff0181401c01
Ruslan Belyi <baiso@ya.ru>
2023-06-09 13:44:41 +0300

    improvements due to team review result

--- packages/client/src/components/ui/input/input.tsx
+++ packages/client/src/components/ui/input/input.tsx
@@ -6,19 +6,39 @@ type InputProps = {
     classNameInput?: string;
     classNameWrap?: string;
     classNameLabel?: string;
-    label: string;
+    label?: string;
     type?: string;
     nameElement: string;
     placeholder?: string;
     onBlur?: FocusEventHandler;
     onFocus?: FocusEventHandler;
 };
-const Input: FC<InputProps> = ({ type = 'text', nameElement, label, placeholder, onBlur, onFocus, classNameInput = '', classNameWrap = '', classNameLabel = '' }) => (
+const Input: FC<InputProps> = ({
+    type = 'text',
+    nameElement,
+    label,
+    placeholder,
+    onBlur,
+    onFocus,
+    classNameInput = '',
+    classNameWrap = '',
+    classNameLabel = '',
+}) => (
     <div className={`${style['input-wrapper']} ${classNameWrap}`}>
-        <label className={`${style.label} ${classNameLabel}`} htmlFor={nameElement}>
-            {label}
-        </label>
-        <input className={`${style.input} ${classNameInput}`} onBlur={onBlur} onFocus={onFocus} type={type} name={nameElement} id={nameElement} placeholder={placeholder} />
+        {label && (
+            <label className={`${style.label} ${classNameLabel}`} htmlFor={nameElement}>
+                {label}
+            </label>
+        )}
+        <input
+            className={`${style.input} ${classNameInput}`}
+            onBlur={onBlur}
+            onFocus={onFocus}
+            type={type}
+            name={nameElement}
+            id={nameElement}
+            placeholder={placeholder}
+        />
     </div>
 );
 export default Input;
--- packages/client/src/helpers/dateformat.ts
+++ packages/client/src/helpers/dateformat.ts
@@ -1,6 +1,6 @@
 export enum FormatType {
-    DATE = '01 января 2023 01:01',
-    DATE_TIME = '01 января 2023',
+    DATE_TIME = '01 января 2023 01:01',
+    DATE = '01 января 2023',
 }
 
 export function dateFormat(stringDate: string, format: FormatType): string | undefined {
@@ -8,17 +8,34 @@ export function dateFormat(stringDate: string, format: FormatType): string | und
 
     const dd = date.getDate();
 
-    const monthNames = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    const monthNames = [
+        'января',
+        'февраля',
+        'марта',
+        'апреля',
+        'мая',
+        'июня',
+        'июля',
+        'августа',
+        'сентября',
+        'октября',
+        'ноября',
+        'декабря',
+    ];
     const monthName = monthNames[date.getMonth()];
 
     const yyyy = date.getFullYear();
 
     let hh: string;
 
-    date.getHours() < 10 ? (hh = `0${date.getHours().toString()}`) : (hh = date.getHours().toString());
+    date.getHours() < 10
+        ? (hh = `0${date.getHours().toString()}`)
+        : (hh = date.getHours().toString());
 
     let min: string;
-    date.getMinutes() < 10 ? (min = `0${date.getMinutes().toString()}`) : (min = date.getMinutes().toString());
+    date.getMinutes() < 10
+        ? (min = `0${date.getMinutes().toString()}`)
+        : (min = date.getMinutes().toString());
 
     if (format === FormatType.DATE_TIME) {
         return `${dd} ${monthName} ${yyyy} ${hh}:${min}`;
