47f679cc937ee27ff6aaffa8c9c41b3f0a6d223e
msn <rpserjo@gmail.com>
2023-06-12 01:14:52 +0300

    добавлена минимальная интеграция с API Яндекс.практикума для реализации задачи

---
+++ packages/client/src/api/api.ts
@@ -0,0 +1,24 @@
+const HOST = 'https://ya-praktikum.tech/api/v2';
+const API = {
+    HOST,
+    RESOURCES: `${HOST}/resources`,
+    RESOURCES_UPLOAD: '/resources',
+    ENDPOINTS: {
+        AUTH: {
+            ENDPOINT: '/auth',
+            SIGNUP: '/signup',
+            SIGNIN: '/signin',
+            USER: '/user',
+            LOGOUT: '/logout',
+        },
+        USER: {
+            ENDPOINT: '/user',
+            PROFILE: '/profile',
+            AVATAR: '/profile/avatar',
+            PASSWORD: '/password',
+            SEARCH: '/search',
+        },
+    },
+};
+
+export default API;
