c01d7bb4c36fe3247d07443c8578fa2fc8285d99
Ruslan Belyi <baiso@ya.ru>
2023-06-11 23:32:44 +0300

    Add ENUM to linkpath in laypout

--- packages/client/src/layout/default/layout.tsx
+++ packages/client/src/layout/default/layout.tsx
@@ -7,6 +7,21 @@ type TLayoutProps = {
     showHeader?: boolean;
 };
 
+enum Route {
+    Landing = '/',
+    Signup = '/signup',
+    Signin = '/signin',
+    Home = '/home',
+    Game = '/game',
+    GameStart = '/game/start',
+    GameFinish = '/game/finish',
+    Profile = '/profile',
+    Leaderboard = '/leaderboard',
+    Forum = '/forum',
+    NotFound = '/wrong-path',
+    ServerErr = '/500',
+}
+
 const Layout: FC<TLayoutProps> = ({ showHeader = true }) => (
     <>
         <div className={style.wrapper}>
@@ -15,18 +30,18 @@ const Layout: FC<TLayoutProps> = ({ showHeader = true }) => (
         </div>
 
         <div className={style['bottom-links']}>
-            <Link to="/">Landing</Link>
-            <Link to="/signup">Sign up</Link>
-            <Link to="/signin">Sign in</Link>
-            <Link to="/home">Home</Link>
-            <Link to="/game">Game</Link>
-            <Link to="/game/start">Game start</Link>
-            <Link to="/game/finish">Game finish</Link>
-            <Link to="/profile">Profile</Link>
-            <Link to="/leaderboard">Leaderboard</Link>
-            <Link to="/forum">Forum</Link>
-            <Link to="/wrong-path">404</Link>
-            <Link to="/500">500</Link>
+            <Link to={Route.Landing}>Landing</Link>
+            <Link to={Route.Signup}>Sign up</Link>
+            <Link to={Route.Signin}>Sign in</Link>
+            <Link to={Route.Home}>Home</Link>
+            <Link to={Route.Game}>Game</Link>
+            <Link to={Route.GameStart}>Game start</Link>
+            <Link to={Route.GameFinish}>Game finish</Link>
+            <Link to={Route.Profile}>Profile</Link>
+            <Link to={Route.Leaderboard}>Leaderboard</Link>
+            <Link to={Route.Forum}>Forum</Link>
+            <Link to={Route.NotFound}>404</Link>
+            <Link to={Route.ServerErr}>500</Link>
         </div>
     </>
 );
