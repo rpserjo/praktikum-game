8b3b80fc050c5c14f33438e220090390ceea0aa9
msn <rpserjo@gmail.com>
2023-06-17 00:09:56 +0300

    Добавлена проверка данных пользователя при первой загрузке

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Loader } from '@ui';
-import Router from '@/router/router';
 import '@/assets/style.scss';
 import { store } from '@/store';
+import App from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <React.Suspense fallback={<Loader />}>
-                    <Router />
-                </React.Suspense>
-            </BrowserRouter>
+            <App />
         </Provider>
     </React.StrictMode>
 );
