6121aaf247c001f4202a1c3d42dc19d73c0f5630
Ruslan Belyi <baiso@ya.ru>
2023-06-18 10:26:31 +0300

    Add service worker by vite-plugin-pwa

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 dotenv.config();
 
@@ -21,5 +22,16 @@ export default defineConfig({
     define: {
         __SERVER_PORT__: process.env.SERVER_PORT,
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            workbox: {
+                globPatterns: ['**/*.{ts,html,js,css,tsx,scss,woff2,png,svg,jpg,js}'],
+            },
+        }),
+    ],
 });
