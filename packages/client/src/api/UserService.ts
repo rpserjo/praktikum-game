b56059f8710b4a1d45eff79290ad7af7b1823ab0
Ruslan Belyi <baiso@ya.ru>
2023-07-09 21:25:13 +0300

    add redux and staticrouter to ssr

---
+++ packages/client/src/api/UserService.ts
@@ -0,0 +1,15 @@
+import { TUser } from '@/store/slices/userSlice';
+
+export interface UserRepository {
+    getUserData(): Promise<TUser>;
+}
+
+export class UserService {
+    // eslint-disable-next-line
+    // eslint-disable-next-line
+    constructor(private repo: UserRepository) {}
+
+    getUserData() {
+        return this.repo.getUserData();
+    }
+}
