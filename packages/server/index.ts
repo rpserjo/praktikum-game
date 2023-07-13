a22442a2ebc609e3966cfd44ee15ec84ac4045bb
msn <rpserjo@gmail.com>
2023-07-13 21:50:19 +0300

    docker

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,9 +13,12 @@ async function startServer() {
     const app = express();
     const port = Number(process.env.SERVER_PORT) || 3001;
     let vite: ViteDevServer | undefined;
-    const distPath = path.dirname(require.resolve('client/dist/index.html'));
-    const srcPath = path.dirname(require.resolve('client'));
-    const ssrClientPath = require.resolve('client/ssr-dist/client.cjs');
+    // const distPath = path.dirname(require.resolve('client/dist/index.html'));
+    // const srcPath = path.dirname(require.resolve('client'));
+    // const ssrClientPath = require.resolve('client/ssr-dist/client.cjs');
+    const distPath = path.resolve(__dirname, '../../client/dist');
+    const srcPath = path.resolve(__dirname, '../../client');
+    const ssrClientPath = path.resolve(__dirname, '../../client/ssr-dist/client.cjs');
 
     if (isDev()) {
         vite = await createViteServer({
