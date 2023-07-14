90cc7ec78904264013abef1dd2e10aa5eb31c575
RuslanBely <126679894+RuslanBely@users.noreply.github.com>
2023-07-14 20:29:48 +0300

    Feature/rooms 42 ssr redux and static router (#32)
    Добавлена работа с хранилищем redux  и статические роуты для рендеринга на сервере.
    
    Изменены сервис-воркеры для стабильной работы с SSR
    
    Добавлена информация по запуску SSR в README.md

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const { off } = require("process");
-
 module.exports = {
     env: {
         browser: true,
@@ -43,13 +41,13 @@ module.exports = {
         'import/prefer-default-export': 'off',
         'no-param-reassign': 'off',
         'max-classes-per-file': 'off',
-        'camelcase': 'off',
-        "no-useless-constructor": "off",
-        "@typescript-eslint/no-useless-constructor": "error",
-        "no-empty-function": "off",
-        "@typescript-eslint/no-empty-function": "error",
-        "class-methods-use-this":"off",
-        "newline-per-chained-call":"off",
+        camelcase: 'off',
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'error',
+        'class-methods-use-this': 'off',
+        'newline-per-chained-call': 'off',
 
     },
 };
