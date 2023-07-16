f0052bb77ef4d58dd033e5ea898184b7b9091eb9
Oleg Peunov <olegpeunov@yandex.ru>
2023-07-16 11:34:52 +0300

    useState for loaded data

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from '@/store/slices/userSlice';
 import gameReducer from '@/store/slices/gameSlice';
-import leaderBoardReducer from '@/store/slices/leaderBoardSlice';
 
 const rootReducer = combineReducers({
     user: userReducer,
     game: gameReducer,
-    leaderBoard: leaderBoardReducer,
 });
 
 export const store = configureStore({
