34279150e4fab6d08232ee56dff52b0dcf3e87c9
Ruslan Belyi <baiso@ya.ru>
2023-07-21 18:00:19 +0000

    add user and auth servise and models

--- packages/server/servises/reply-service.ts
+++ packages/server/servises/reply-service.ts
@@ -4,17 +4,6 @@ class ReplyService {
     async createReply(message: string, CommentId: number, author: string) {
         return Reply.create({ message, CommentId, author });
     }
-
-    async findReplysForComment(commentId: number, isOrderUpdatedASC = false) {
-        const UpdatedOrder = isOrderUpdatedASC ? 'ASC' : 'DESC';
-
-        return Reply.findAndCountAll({
-            where: {
-                CommentId: commentId,
-            },
-            order: [['updatedAt', UpdatedOrder]],
-        });
-    }
 }
-const commentService = new ReplyService();
-export default commentService;
+const replyService = new ReplyService();
+export default replyService;
