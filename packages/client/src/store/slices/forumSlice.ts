2cbf8bead4dba52467c0c60beb9f536ba54bb3ce
Hellga2023 <helllga2018@yandex.ru>
2023-07-22 12:34:37 +0400

    Feature/ROOMS-54 forum slice fixed

--- packages/client/src/store/slices/forumSlice.ts
+++ packages/client/src/store/slices/forumSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { TTopicInfo } from '@/types/data-types';
 
 type TForum = {
@@ -17,6 +17,15 @@ const initialState: TState = {
     },
 };
 
+interface IForumService {
+    getForumTopics(): Promise<TTopicInfo[]>;
+}
+
+const loadForumTopics = createAsyncThunk<TTopicInfo[]>('root/AuthUser', async (_, thunkApi) => {
+    const service: IForumService = thunkApi.extra as IForumService;
+    return service.getForumTopics();
+});
+
 const forumSlice = createSlice({
     name: 'forum',
     initialState,
@@ -24,6 +33,21 @@ const forumSlice = createSlice({
         setForumTopics(state, action) {
             state.forum.forumTopics = action.payload;
         },
+    },
+    extraReducers: builder => {
+        /* builder.addCase(loadUser.pending, state => {
+            state.isLoaded = false;
+            state.isLoading = true;
+        });
+        builder.addCase(loadUser.rejected, state => {
+            state.isLoaded = true;
+            state.user = null;
+            state.isLoading = false;
+        }); */
+        builder.addCase(loadForumTopics.fulfilled, (state, action) => {
+            const { payload } = action;
+            state.forum.forumTopics = payload;
+        });
     },
 });
 
