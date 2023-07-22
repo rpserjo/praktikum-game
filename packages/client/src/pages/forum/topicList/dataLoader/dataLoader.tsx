c802923711dafac8b8cf6caebaf2e4794b0e4aef
Hellga2023 <helllga2018@yandex.ru>
2023-07-22 11:18:32 +0400

    Feature/ROOMS-54 getting from mock server moved to api, trying using redux

--- packages/client/src/pages/forum/topicList/dataLoader/dataLoader.tsx
+++ packages/client/src/pages/forum/topicList/dataLoader/dataLoader.tsx
@@ -1,26 +1,36 @@
 import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
-import MockServer from '@/mocks/mock-server';
 import Page from '@/pages/forum/topicList/page/page';
 import { Loader } from '@/components/ui';
-// import ForumApi from '@/api/ForumApi';
-// import { TTopicInfo } from '@/types/data-types';
+// import { setForumTopics } from '@/store/slices/forumSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { RootState } from '@/store';
+
+import ForumApi from '@/api/ForumApi';
 
 const Forum: FC = () => {
     const { page = 1 } = useParams();
 
     const MAX_ELEMENTS_PER_PAGE = 10;
-    // todo remove and review models
-    const server = new MockServer();
-    const serverData = server.getTopicListNew(+page, MAX_ELEMENTS_PER_PAGE);
 
-    /* const forumApi = new ForumApi();
-    const topicDataReal = forumApi.getTopics(+page, MAX_ELEMENTS_PER_PAGE); */
+    const forumApi = new ForumApi();
+    const forumData = forumApi.getTopics(+page, MAX_ELEMENTS_PER_PAGE);
 
-    if (!serverData) {
+    // todo where should we do this?
+    /* const dispatch = useDispatch();
+    const forumState = useSelector((state: RootState) => state.forum);
+    const { forum } = forumState;
+    dispatch(
+        setForumTopics({
+            ...forum,
+            forumTopics: forumData.topics,
+        })
+    ); */
+
+    if (!forumData) {
         return <Loader />;
     }
-    return <Page topics={serverData.topics} page={+page} lastPage={serverData.lastPage} />;
+    return <Page topics={forumData.topics} page={+page} lastPage={forumData.lastPage} />;
 };
 
 export default Forum;
