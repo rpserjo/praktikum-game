3c72ac4e37325f0ef165550aea772c67cd189e6d
Ruslan Belyi <baiso@ya.ru>
2023-08-04 16:57:12 +0000

    modified jest config for support mp3 stub

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -15,9 +15,11 @@ export default {
     },
     moduleNameMapper: {
         '\\.(css|scss)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        'jest-transform-stub',
         '^@pages/(.*)$': '<rootDir>/src/pages/$1',
         '^@components/(.*)$': '<rootDir>/src/components/$1',
-        '^@ui/(.*)$': '<rootDir>/src/components/ui/$1',
+        '^@ui(.*)$': '<rootDir>/src/components/ui/$1',
         '^@/(.*)$': '<rootDir>/src/$1',
     },
 };
