34734a997e8f18fa13d332c0595b4b261f678915
msn <rpserjo@gmail.com>
2023-08-15 15:51:43 +0300

    bottom links removed

--- packages/client/src/layout/default/layout.tsx
+++ packages/client/src/layout/default/layout.tsx
@@ -1,33 +1,17 @@
 import React, { FC } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '@/components/header/header';
 import style from './layout.module.scss';
-import { RouteNames } from '@/router/router';
 
 type TLayoutProps = {
     showHeader?: boolean;
 };
 
 const Layout: FC<TLayoutProps> = ({ showHeader = true }) => (
-    <>
-        <div className={style.wrapper}>
-            {showHeader && <Header />}
-            <Outlet />
-        </div>
-
-        <div className={style['bottom-links']}>
-            <Link to={RouteNames.LANDING}>Landing</Link>
-            <Link to={RouteNames.SIGNUP}>Sign up</Link>
-            <Link to={RouteNames.SIGNIN}>Sign in</Link>
-            <Link to={RouteNames.HOME}>Home</Link>
-            <Link to={RouteNames.GAME}>Game</Link>
-            <Link to={RouteNames.PROFILE}>Profile</Link>
-            <Link to={RouteNames.LEADERBOARD}>Leaderboard</Link>
-            <Link to={RouteNames.FORUM}>Forum</Link>
-            <Link to={RouteNames.NOT_FOUND}>404</Link>
-            <Link to={RouteNames.SERVER_ERROR}>500</Link>
-        </div>
-    </>
+    <div className={style.wrapper}>
+        {showHeader && <Header />}
+        <Outlet />
+    </div>
 );
 
 export default Layout;
